b8893470133205cc856649f1f2636c621612aefa
eryalee <lierya1987@sina.cn>
2013-11-23 16:33:28 +0800

    struct big&little endian pack, unpack

---
+++ test_struct.py
@@ -0,0 +1,16 @@
+
+
+import struct
+
+print
+print '==========pack - unpack==========='
+
+str = struct.pack("<ii", 20, 400)
+str1 = struct.pack(">ii", 20, 400)
+print str
+print str1
+print repr(str)
+print repr(str1)
+
+print struct.calcsize("iic")
+print struct.calcsize("4i")
